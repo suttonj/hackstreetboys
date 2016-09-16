67f7f486f6bc4a1a21a3ad2e7fd6e96636685aa5
Kyle Silberbauer <kyle.silberbauer@gmail.com>
2016-09-16 19:39:38 -0400

    loading page

--- src/client/client.js
+++ src/client/client.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, Route, Redirect, browserHistory, useRouterHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory, useRouterHistory } from 'react-router';
 import { createBrowserHistory } from 'history';
 
 import * as reducers from './reducers';
 
 import App from './components/App';
+import Loading from './components/Loading';
 import Chat from './components/Chat';
 import Tools from './components/Tools';
 import Profile from './components/Profile';
@@ -27,8 +28,8 @@ ReactDOM.render(
     <Provider store={store}>
         { /* Tell the Router to use our enhanced history */ }
         <Router history={browserHistory}>
-            <Redirect from="/" to="chat" />
             <Route path="/" component={App}>
+                <IndexRoute component={Loading} />
                 <Route path="chat" component={Chat} />
                 <Route path="tools" component={Tools} />
                 <Route path="profile" component={Profile} />
--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 import Header from './Header';
 
 export default class App extends React.Component {
+
+    componentDidMount() {
+        if (window.location.pathname === `/`) {
+            setTimeout(() => window.location = `/chat`, 5000);
+        }
+    }
+
     render() {
         return (
             <div>
