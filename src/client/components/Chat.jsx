14a017d233e6997457bafe934a040f83b719606c
Kyle Silberbauer <kyle.silberbauer@gmail.com>
2016-09-17 00:02:24 -0400

    working chat app PoC

--- src/client/components/Chat.jsx
+++ src/client/components/Chat.jsx
@@ -4,8 +4,17 @@ export default class Chat extends React.Component {
     render() {
         return (
             <div>
-                Chat
+                <ul id="messages"></ul>
+                <form action="">
+                    <input id="m" autoComplete="off" /><button>Send</button>
+                </form>
             </div>
         );
+    }
+}
+
+const styles = {
+    messages: {
+        listStyletType: `none`,
     }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import http from 'http';
+import socketIo from 'socket.io';
 
 const app = express();
 const httpServer = http.Server(app);
+const io = socketIo(httpServer);
 
 app.get(`/client`, (req, res) => {
     res.sendFile(`${__dirname}/client.html`);
@@ -10,6 +12,15 @@ app.get(`/client`, (req, res) => {
 
 app.get(`/host`, (req, res) => {
     res.sendFile(`${__dirname}/host.html`);
+});
+
+io.on(`connection`, socket => {
+    console.log(`a user connected`);
+    socket.on(`disconnect`, () => console.log(`user disconnected`));
+    socket.on(`chat message`, msg => {
+        // socket.broadcast.emit(`chat message`, msg); // send to everyone except sender
+        io.emit(`chat message`, msg);
+    });
 });
 
 httpServer.listen(3001, () => console.log('listening on *:3001'));
