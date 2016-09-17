820804013a9d41c682afffacf1afc2b38f041a80
Kyle Silberbauer <kyle.silberbauer@gmail.com>
2016-09-17 00:06:28 -0400

    database

--- src/server/index.js
+++ src/server/index.js
@@ -5,19 +5,17 @@ import socketIo from 'socket.io';
 const app = express();
 const httpServer = http.Server(app);
 const io = socketIo(httpServer);
+const db = { messages: [] };
 
-app.get(`/client`, (req, res) => {
-    res.sendFile(`${__dirname}/client.html`);
-});
-
-app.get(`/host`, (req, res) => {
-    res.sendFile(`${__dirname}/host.html`);
-});
+app.get(`/client`, (req, res) => res.sendFile(`${__dirname}/client.html`));
+app.get(`/host`, (req, res) => res.sendFile(`${__dirname}/host.html`));
 
 io.on(`connection`, socket => {
     console.log(`a user connected`);
+    db.messages.forEach(msg => socket.emit(`chat message`, msg));
     socket.on(`disconnect`, () => console.log(`user disconnected`));
     socket.on(`chat message`, msg => {
+        db.messages.push(msg);
         // socket.broadcast.emit(`chat message`, msg); // send to everyone except sender
         io.emit(`chat message`, msg);
     });
