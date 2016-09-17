2c934e43d617e46c1bed58415a4e671e036cd271
Kyle Silberbauer <kyle.silberbauer@gmail.com>
2016-09-16 21:09:32 -0400

    react-swipeable-views + material-ui

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,21 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Tabs from 'material-ui/Tabs/Tabs';
+import Tab from 'material-ui/Tabs/Tab';
+import SwipeableViews from 'react-swipeable-views';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Checkbox from 'material-ui/Checkbox';
 
-import Tabs from './Tabs';
+const styles = {
+    slide: {
+        padding: 15,
+        minHeight: 100,
+        color: `black`,
+    },
+    slide1: {
+        backgroundColor: `white`,
+    },
+    slide2: {
+        backgroundColor: `white`,
+    },
+    slide3: {
+        backgroundColor: `white`,
+    },
+};
 
-export default class App extends React.Component {
+const muiTheme = getMuiTheme({
+    palette: {
+        textColor: `black`,
+        alternateTextColor: `black`,
+        primary1Color: `white`,
+        accent1Color: `darkgray`,
+    },
+});
 
-    componentDidMount() {
-        if (window.location.pathname === `/`) {
-            setTimeout(() => window.location = `/chat`, 5000);
-        }
+export default class App extends Component {
+
+    state = {
+        index: 0,
+    }
+
+    constructor(props) {
+        super(props);
+        this.handleChangeTabs = this.handleChangeTabs.bind(this);
+        this.handleChangeIndex = this.handleChangeIndex.bind(this);
+    }
+
+    handleChangeTabs = value => {
+        this.setState({
+            index: value,
+        });
+    }
+
+    handleChangeIndex = index => {
+        this.setState({ index });
     }
 
     render() {
+        const {
+            index,
+        } = this.state;
+
         return (
-            <div>
-                { window.location.pathname !== `/` && <Tabs /> }
-                {this.props.children}
-            </div>
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <div>
+                    <Tabs value={index}>
+                        <Tab label="Chat" value={0} onClick={() => this.handleChangeTabs(0)} />
+                        <Tab label="Tools" value={1} onClick={() => this.handleChangeTabs(1)} />
+                        <Tab label="Profile" value={2} onClick={() => this.handleChangeTabs(2)} />
+                    </Tabs>
+                    <SwipeableViews index={index} onChangeIndex={this.handleChangeIndex}>
+                        <div style={{ ...styles.slide, ...styles.slide1 }}>
+                            Kyle: Said something
+                        </div>
+                        <div style={{ ...styles.slide, ...styles.slide2 }}>
+                            <Checkbox label="test event propogation" />
+                        </div>
+                        <div style={{ ...styles.slide, ...styles.slide3 }}>
+                            Name: Kyle Silberbauer
+                        </div>
+                    </SwipeableViews>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
