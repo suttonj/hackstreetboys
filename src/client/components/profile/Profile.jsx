63790838054fa2f185652523354480c78f145787
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2016-09-18 19:07:50 +0200

    add some stuff to the profile page

--- src/client/components/profile/Profile.jsx
+++ src/client/components/profile/Profile.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import { connect } from 'react-redux';
+import React, {PropTypes} from 'react';
 
-export default class Profile extends React.Component {
+class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.updateName = this.updateName.bind(this);
+    }
+
+    updateName(event) {
+        const newName = event.target.value;
+        this.props.dispatch({type: `UPDATE_PROFILE`, name: newName})
+    }
+
     render() {
         return (
             <div>
-                Profile
+                <h1> Profile </h1>
+                <input autoComplete="off" placeholder="Enter Your Name" onChange={this.updateName} value={this.props.name}/>
+                <hr></hr>
+                <h2>Want your own join.me account?</h2>
+                <form>
+                    <div>
+                        <label>Email</label>
+                        <input autoComplete="off" value={this.props.email}/>
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input autoComplete="off" type="password"/>
+                    </div>
+                    <button type='submit'>Register</button>
+                </form>
             </div>
         );
     }
 }
+
+Profile.propTypes = {
+    name : PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+}
+
+export default connect(state => state.profile)(Profile);
