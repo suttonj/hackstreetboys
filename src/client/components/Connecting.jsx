0014a18721b75012591c270977227e36108b5199
Ean Moody <moody.designer@gmail.com>
2016-09-19 17:37:07 +0200

    Kickass loading screen

--- src/client/components/Connecting.jsx
+++ src/client/components/Connecting.jsx
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 export default class Connecting extends Component {
     render() {
         return (
-            <h1 onClick={this.props.onClick}>Connecting...</h1>
+          <div className="listen-spinner">
+            <h2 onClick={this.props.onClick}>Listening For a Meeting...</h2>
+            <div id="scenes">
+              <div className="scene scene1">
+                <div className="description">Giant Robots!</div>
+                <div className="dot"></div>
+              </div>
+            </div>
+          </div>
         );
     }
 }
