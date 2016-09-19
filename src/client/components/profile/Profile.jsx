f2a04bffa8ad4c1ef11c82e76433bcb42bca9475
Jeremy Sutton <jeremy.sutton@logmein.com>
2016-09-19 03:55:28 -0400

    avatar to profile

--- src/client/components/profile/Profile.jsx
+++ src/client/components/profile/Profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Background from './Background';
+import Avatar from './Avatar';
 
 export default class Profile extends React.Component {
     render() {
         return (
             <div>
-                <Background />
+                <Background backgroundImage={this.props.data.background} />
+                <Avatar profileImage={this.props.data.avatar} avatarUploadEvent={this.props.setAvatar} />
             </div>
         );
     }
