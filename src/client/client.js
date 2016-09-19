c798c57b0b8403a4fa1bd7f5cd5956803a434b8d
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2016-09-19 14:23:46 +0200

    get audio connection to work for real!

--- src/client/client.js
+++ src/client/client.js
@@ -1,3 +1,4 @@
+const SonicServer = require('~/shared/libs/sonic-server');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
@@ -9,12 +10,25 @@ import * as reducers from './reducers';
 
 import Client from './components/Client';
 
+import {ALPHABET} from '~/shared/constants/audio';
+
 // Add the reducer to your store on the `routing` key
 const store = createStore(
     combineReducers(reducers),
     {},
     compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f)
 );
+
+const sserver = new SonicServer({alphabet: ALPHABET});
+const connected = false;
+sserver.on('message', function(message) {
+  // message is '31415'. Do something with it.
+  if(confirm("Meeting ID received - connect to join.me Engage?")){
+    sserver.stop();
+    store.dispatch({ type: `CONNECT_TO_MEETING` });
+  }
+});
+sserver.start();
 
 io().on(`EMITTED`, data => store.dispatch(data));
 
@@ -24,6 +38,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('mount')
 );
-
-// TODO: connecting logic, probably in actionCreator
-setTimeout(() => store.dispatch({ type: `CONNECT_TO_MEETING` }), 0);
