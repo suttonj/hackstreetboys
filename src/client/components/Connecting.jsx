5ceeaae11aef9b4d9cba0ad24d4e15dc93023af0
Ean Moody <moody.designer@gmail.com>
2016-09-20 10:04:34 +0200

    fix loading screen so it doesn't go gray

--- src/client/components/Connecting.jsx
+++ src/client/components/Connecting.jsx
@@ -4,15 +4,15 @@ export default class Connecting extends Component {
     render() {
         return (
           <div className="listen-spinner">
-            <div>
-              <h2 onClick={this.props.onClick}>Listening For a Meeting...</h2>
-              <Bars loadingString="123412523" />
-            </div>
             <div id="scenes">
               <div className="scene scene1">
                 <div className="description">Giant Robots!</div>
                 <div className="dot"></div>
               </div>
+            </div>
+            <div className="listen-status">
+              <h2 onClick={this.props.onClick}>Listening For a Meeting...</h2>
+              <Bars loadingString="123412523" />
             </div>
           </div>
         );
